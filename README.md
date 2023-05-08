60868baa7035a293d03fcbe10cde30240ab0cddb
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-05-08 06:21:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Transfer with signature in delivery slip
+----------------------------------------------
+This module display the Signature label in the delivery slip. 
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Transfer with signature in delivery slip',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Stock',
+    'demo': [],
+    'depends': ['stock'],
+    'data': [
+        'report/report_deliveryslip.xml',
+    ],
+    'license': 'LGPL-3',
+}
